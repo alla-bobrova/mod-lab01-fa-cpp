ab8d5cce3db8ef9d4ff0dd380773dd68602586e1
alla-bobrova <75272955+alla-bobrova@users.noreply.github.com>
2023-03-06 17:38:02 +0300

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -1,9 +1,9 @@
 // Copyright 2022 UNN-IASR
-#ifndef INCLUDE_FUN_H_
-#define INCLUDE_FUN_H_
+#ifndef FUN_H
+#define FUN_H
 
-unsigned int faStr1(const char *str);
-unsigned int faStr2(const char *str);
-unsigned int faStr3(const char *str);
+unsigned int faStr1(const char* str);
+unsigned int faStr2(const char* str);
+unsigned int faStr3(const char* str);
 
-#endif  // INCLUDE_FUN_H_
+#endif // FUN_H
