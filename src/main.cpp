c8d7a4d880a2bcf0a45be6c099822a56a0d554df
alla-bobrova <75272955+alla-bobrova@users.noreply.github.com>
2023-03-06 17:39:01 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    char str[1000];
+    std::cout << "Enter a string: ";
+    std::cin.getline(str, 1000);
+    std::cout << "Number of words without digits: " << faStr1(str) << std::endl;
+    std::cout << "Number of words starting with uppercase letter and containing only lowercase letters: " << faStr2(str) << std::endl;
+    std::cout << "Average length of words (rounded to the nearest integer): " << faStr3(str) << std::endl;
+
     return 0;
 }
