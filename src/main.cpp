31035edb325a351ef23232285cd835969c287937
alla-bobrova <75272955+alla-bobrova@users.noreply.github.com>
2023-03-07 12:44:06 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ int main() {
     char str[1000];
     std::cout << "Enter a string: ";
     std::cin.getline(str, 1000);
-    std::cout << "Without digits: " << faStr1(str) << std::endl;
-    std::cout << "first uppercase next lowercase letters: " << faStr2(str) << std::endl;
-    std::cout << "Average length of words: " << faStr3(str) << std::endl;
+    std::cout << "task1: " << faStr1(str) << std::endl;
+    std::cout << "task2: " << faStr2(str) << std::endl;
+    std::cout << "task3: " << faStr3(str) << std::endl;
 
     return 0;
 }
