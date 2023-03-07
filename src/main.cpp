497475f170188ebb9fd535daf0d9546561253ff1
alla-bobrova <75272955+alla-bobrova@users.noreply.github.com>
2023-03-07 12:38:38 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ int main() {
     char str[1000];
     std::cout << "Enter a string: ";
     std::cin.getline(str, 1000);
-    std::cout << "Number of words without digits: " << faStr1(str) << std::endl;
-    std::cout << "Number of words starting with uppercase letter and containing only lowercase letters: " << faStr2(str) << std::endl;
-    std::cout << "Average length of words (rounded to the nearest integer): " << faStr3(str) << std::endl;
+    std::cout << "Without digits: " << faStr1(str) << std::endl;
+    std::cout << "first uppercase next lowercase letters: " << faStr2(str) << std::endl;
+    std::cout << "Average length of words: " << faStr3(str) << std::endl;
 
     return 0;
 }
